37e1cc4ea1d84b382f69da3f0e939b3bf1a14052
Annie Kevra <anniekevra95@gmail.com>
2023-12-19 12:15:17 -0500

    limit_num_of_movies

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -4,10 +4,16 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router.route("/").get(controller.list).all(methodNotAllowed);
 
-router.route("/:movieId([0-9]+)").get(controller.read).all(methodNotAllowed);
+router.route("/:movieId").get(controller.read).all(methodNotAllowed);
 
-router.route("/:movieId/theaters").get(controller.listTheaters).all(methodNotAllowed)
+router
+  .route("/:movieId/theaters")
+  .get(controller.listTheaters)
+  .all(methodNotAllowed);
 
-router.route("/:movieId/reviews").get(controller.listReviews).all(methodNotAllowed)
+router
+  .route("/:movieId/reviews")
+  .get(controller.listReviews)
+  .all(methodNotAllowed);
 
 module.exports = router;
